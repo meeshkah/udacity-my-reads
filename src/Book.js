aa9f55309f777008815db0cd4654691d9d7bf167
Misha Rumbesht <m.rumbesht@gmail.com>
2017-07-22 17:06:45 +0100

    refactor: Separate styles into module files
    Break up App.css into multiple module styles; add .is-in-library styles

--- src/Book.js
+++ src/Book.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Book.css';
 
 class Book extends Component {
   state = {
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './SearchBooks.css';
 import { Link } from 'react-router-dom';
 import * as BooksAPI from './BooksAPI';
 
